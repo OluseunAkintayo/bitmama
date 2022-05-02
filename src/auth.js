d637aea5b9cbd1276430d41335e7b97ce2f2fd11
unknown <oluseun.akintayo@gmail.com>
2022-05-02 13:00:32 +0100

    added supabase url

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const { REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_ANON_KEY } = process.env;
 
-const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_ANON_KEY);
+const supabase = createClient("https://ggynidmjnmiltiascrmv.supabase.co", REACT_APP_SUPABASE_ANON_KEY);
 
 export { supabase };
